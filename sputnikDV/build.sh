90b24aca79375458cbefe3968e79c6d701e29c52
kesfet <kesfet@zohomail.in>
2026-03-31 13:33:06 +0000

    Update build.sh

--- sputnikDV/build.sh
+++ sputnikDV/build.sh
@@ -1,1 +1,15 @@
+#=================================================+
+#        Executable For /deltav.s         V0
+#=================================================+
 
+#!/bin/bash
+file="deltav"
+
+nasm -f elf64 "$file.s" -o "$file.o"
+ld "$file.o" -o "$file"         
+
+if [ $? -eq 0 ]; then
+    echo "Build successful: ./$file"
+else
+    echo "Build failed"
+fi
